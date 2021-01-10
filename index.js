824350378302e6e1707acf1731ce57feab0f2b61
rcecile <rcecile@reseau.free.fr>
2021-01-10 16:06:43 +0100

    LOGIN > add new route for login and subscription

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const multerS3 = require("multer-s3");
 const client = require("./app/db");
 const express = require("express");
 const app = express();
-// const session = require("express-session");
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 app.use(express.static(path.join(__dirname, "public")));
 // recupérer les donnés dans le body (pour les requete POST et PATCH)
@@ -31,30 +31,30 @@ const bodyParser = multer();
 // on utlise .none() pour dire à multer qu'on attends pas de fichier, uniquement des inputs "classiques" !
 app.use(bodyParser.none());
 
-// const store = new SequelizeStore({
-// 	db: client,
-// 	tableName: "session",
-// });
+const store = new SequelizeStore({
+	db: client,
+	tableName: "session",
+});
 
-// app.set("trust proxy", false);
-// app.use(
-// 	session({
-// 		secret: process.env.SESSION_SECRET,
-// 		store,
-// 		saveUninitialized: true,
-// 		resave: false, // we support the touch method so per the express-session docs this should be set to false
-// 		// proxy: false, // if you do SSL outside of node.
-// 		expire: 5000 * 60 * 60,
-// 		cookie: {
-// 			secure: true,
-// 			httpOnly: false,
-// 			maxAge: 5000 * 60 * 60,
-// 			sameSite: "none",
-// 		},
-// 	})
-// );
+app.set("trust proxy", false);
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		store,
+		saveUninitialized: true,
+		resave: false, // we support the touch method so per the express-session docs this should be set to false
+		// proxy: false, // if you do SSL outside of node.
+		expire: 5000 * 60 * 60,
+		cookie: {
+			secure: false,
+			httpOnly: false,
+			maxAge: 5000 * 60 * 60,
+			sameSite: "none",
+		},
+	})
+);
 
-// store.sync();
+store.sync();
 
 // aws.config.update({
 // 	secretAccessKey: process.env.AWS_SECRET_ACCES_KEY,
